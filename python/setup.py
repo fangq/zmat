cb40e0f62b5b8c18dac129b104fde0beef924126
Qianqian Fang <fangqq@gmail.com>
2026-04-10 23:26:31 -0400

    [deploy] build zmat with static gcc

--- python/setup.py
+++ python/setup.py
@@ -245,6 +245,7 @@ if platform.system() == "Windows":
     extra_compile_args = ["/O2"]
 else:
     extra_compile_args.append("-fPIC")
+    extra_link_args.extend(["-static-libgcc", "-static-libstdc++"])
     if platform.system() == "Darwin":
         extra_link_args.extend(["-undefined", "dynamic_lookup"])
     else:
